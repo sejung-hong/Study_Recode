0b18083f61e102900ca63c8ddda53e6e73450c4e
sejung-hong <sejungh@naver.com>
2021-05-19 18:29:18 +0900

    21.05.19

---
+++ 백준(C++)/이외/6588.cpp
@@ -0,0 +1,43 @@
+//6588 ∞ÒµÂπŸ»Â¿« √ﬂ√¯
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool Prime(int num) {
+	for (int i = 2; i <= sqrt(num); i++) {
+		if (num % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	while (1) {
+		cin >> n;
+		if (n == 0) {
+			break;
+		}
+
+		int check = 0;
+
+		for (int i = 3; i < n; i+=2) {
+			if (Prime(i) && Prime(n-i)) {
+				cout << n << " = " << i << " + " << n-i << "\n";
+				check = 1;
+				break;
+			}
+		}
+
+		if (check == 0) {
+			cout << "Goldbach's conjecture is wrong." << "\n";
+		}
+
+	}
+
+	return 0;
+}
