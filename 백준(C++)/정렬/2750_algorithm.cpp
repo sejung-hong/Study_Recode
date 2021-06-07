2dba34936b0215c7ddc04bebc6eafddf3420e8db
sejung-hong <sejungh@naver.com>
2021-06-07 10:11:43 +0900

    2106.07

---
+++ 백준(C++)/정렬/2750_algorithm.cpp
@@ -0,0 +1,30 @@
+//2750 수 정렬하기 알고르즘 헤더 사용
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	vector <int> v;
+
+	int num;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		v.push_back(num);
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < N; i++) {
+		cout << v[i] << "\n";
+	}
+
+
+	return 0;
+}
