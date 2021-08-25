c85bb23624c00324bc4160a86b8d362b6351eadb
sejung-hong <sejungh@naver.com>
2021-08-25 10:11:29 +0900

    21.08.25

---
+++ 프로그래머스/level3/N으로 표현.cpp
@@ -0,0 +1,18 @@
+//프로그래머스 N으로 표현
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int N, int number) {
+    int answer = 0;
+    return answer;
+}
+
+int main() {
+    int N = 5;
+    int number = 12;
+    cout << solution(N, number) << "\n";
+    return 0;
+}
