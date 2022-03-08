2901759c45cc5120f1df5f4373abba65e4bf814d
sejung-hong <sejungh@naver.com>
2022-03-09 01:43:47 +0900

    day : 22.03.09

---
+++ 백준(C++)/Silver/상어 초등학교_21608.cpp
@@ -0,0 +1,16 @@
+//백준 21608 상어 초등학교
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+
+	return 0;
+}
